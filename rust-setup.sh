5c80ed47c36b86d0146893d51cb8791fcd1e3347
Quinn Langille <quinn@quinnlangille.com>
2019-11-03 14:29:26 -0500

    feat: add rust setup script

---
+++ rust-setup.sh
@@ -0,0 +1,20 @@
+echo "⬇️ Installing rustlang support"
+
+# Install rustlang tooling
+if !(command -v brew &>/dev/null); then
+  echo "Please install HomeBrew!";
+else 
+  brew install rustup;
+  rustup-init;
+  echo "Rust installed at $(rustc --version)";
+  echo "cargo installed at $(cargo --version)";
+fi
+
+# racer for syntax
+# rustfmt for formatting
+# rustsym for symbol support
+# cargo-watch for hot reload
+cargo install racer rustfmt rustsym cargo-watch
+
+echo "👏 Rust setup complete!"
+
