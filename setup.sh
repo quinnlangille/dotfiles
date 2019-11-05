25c8d5f22bd2b3182187d8aee03c7541b7dc02f4
Quinn Langille <quinn@quinnlangille.com>
2019-11-05 12:41:03 -0500

    feat: add set up for tmux plugins

--- setup.sh
+++ setup.sh
@@ -67,6 +67,19 @@ else
     brew install tmux;
 fi
 
+# Set up plugin manager and install plugins
+# Download tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# start a server but don't attach to it
+tmux start-server
+# create a new session but don't attach to it either
+tmux new-session -d
+# install the plugins
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
+# killing the server is not required, I guess
+tmux kill-server
+
 # Set up YouCompleteMe
 if command -v cmake &>/dev/null; then
     echo "Cmake already installed at $(which cmake)";
